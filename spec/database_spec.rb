650d1d97a50039f6076be6b40d638beae151d892
Fabien Arcellier <farcellier@octo.com>
2016-10-12 22:37:26 +0200

    feat: correction of challenge 2

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe "database" do
+  it "needs to have the ntp service installed and running" do
+    expect(package('ntp')).to be_installed
+    expect(service('ntp')).to be_running
+  end
 end
--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -4,4 +4,9 @@ describe "webserver" do
   it "needs to have the port 80 listening" do
     expect(port('80')).to be_listening
   end
+
+  it "needs to have the ntp service installed and running" do
+    expect(package('ntp')).to be_installed
+    expect(service('ntp')).to be_running
+  end
 end
