33ce2bbc34300f995d6ed2d9a8573694a21b6393
Fabien Arcellier <farcellier@octo.com>
2016-10-13 12:26:09 +0200

    feat: add user inclusion

--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -9,4 +9,18 @@ describe "webserver" do
     expect(package('ntp')).to be_installed
     expect(service('ntp')).to be_running
   end
+
+  it "needs developpers to use their own accounts" do
+    users = ['far', 'fxv', 'mhe']
+    users.each {|u|
+      expect(user(u)).to exist
+    }
+  end
+
+  it "needs developpers account to have dev as primary group" do
+    users = ['far', 'fxv', 'mhe']
+    users.each {|u|
+      expect(user(u)).to belong_to_primary_group 'dev'
+    }
+  end
 end
